08dc7e5d465cd9346d74fa6d4cf2ba397b481ac5
Anton Orlov <anton_orlov@ukr.net>
2023-04-03 19:19:23 +0300

    hw1 update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node.js
-study rep for testing my skill
+
+study rep for testing skill
+
+HW1 links:
+https://ibb.co/6mrvsYm
+https://ibb.co/mcbnjfm
+https://ibb.co/85BB9LS
+https://ibb.co/yfpXNT3
---
+++ hw1/index.js
@@ -0,0 +1,46 @@
+const { program } = require("commander");
+
+const contactsService = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contactById = await contactsService.getContactById(id);
+      return console.log(contactById);
+
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      return console.log(deleteContact);
+
+    case "update":
+      const updateContact = await contactsService.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+      return console.log(updateContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-em, --email <type>")
+  .option("-ph, --phone <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
